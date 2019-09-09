07e476b6767d821b2a4ada16b24e0ca0140d717b
thoward02-2ndPeriod <36546676+thoward02@users.noreply.github.com>
2019-09-09 09:45:08 -0400

    Push example

---
+++ Js/Buttons.js
@@ -0,0 +1,16 @@
+function CreateButtons(){
+  const Links [];
+
+
+  let Buttons = document.getElementsByClassName("Link");
+  console.log(Buttons)
+
+
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  CreateButtons();
+});
